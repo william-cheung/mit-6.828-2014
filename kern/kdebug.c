e1590dd07e2adae160b9f389d6ed47f87906cf29
William Cheung <escape.the.well@gmail.com>
2016-04-01 13:40:46 +0800

    debuginfo_eip completed!

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -181,7 +181,7 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	stab_binsearch(stabs, &lline, &rline, N_SLINE, addr);
 	
 	if (lline <= rline) {
-		info->eip_line = stabs[lline].n_value;
+		info->eip_line = stabs[lline].n_desc;
 	} else return -1;
 
 
