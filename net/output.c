edd97ce9db38d1363812fcfef94ac01bd9fdb493
William Cheung <escape.the.well@gmail.com>
2016-09-15 11:21:36 +0800

    Update output.c

--- net/output.c
+++ net/output.c
@@ -18,7 +18,7 @@ output(envid_t ns_envid)
         // if ipc value is not NSREQ_OUTPUT or the page we received is not from 
         // the Network Server, continue	
         if (r != NSREQ_OUTPUT || envid != ns_envid) 
-			continue;
+            continue;
         
         // send the packet to the device driver
         while ((r = sys_nic_try_send(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len)) == -E_TX_FULL)
